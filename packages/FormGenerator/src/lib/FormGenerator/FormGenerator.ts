3685927f37eda7a947739741ce912235b655f1cd
amirkhabbax <amir.khabbaz@gmail.com>
2025-12-28 12:19:54 +0330

    monorepo structure first version

---
+++ packages/FormGenerator/src/lib/FormGenerator/FormGenerator.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-form-generator',
+  imports: [],
+  templateUrl: './FormGenerator.html',
+  styleUrl: './FormGenerator.css',
+})
+export class FormGenerator {}
